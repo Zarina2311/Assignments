cfce171b6a3888643e4c8724031ea0af5ae27c3a
Zarina Gumbatova <26606767+Zarina2311@users.noreply.github.com>
2020-01-21 21:50:37 -0800

    Add two exercises

---
+++ movie-night-code-challenge.js
@@ -0,0 +1,27 @@
+//Independent Practice
+
+let potentialAtendees  = ["Priya", "Ming", "David", "Alex", "Breanna"];
+
+function movieNight(listOfYeses) {
+    let confirmedAttendees =[];
+
+if (listOfYeses.includes("David")) {
+    confirmedAttendees.push("David", "Alex");
+} else if (listOfYeses.includes("Ming") || listOfYeses.includes("Priya")) {
+    confirmedAttendees.push("Ming", "Priya");
+}
+//possibly check for Priya - ??
+if (confirmedAttendees.length >=2) {
+    confirmedAttendees.push("Breanna");
+}
+    return confirmedAttendees;
+}
+console.log(movieNight(["Priya", "David", "Alex"]));
+
+
+//order of importance
+//1. David
+//2. Alex
+//3. Ming
+//4. Priya
+//5. Breanna
