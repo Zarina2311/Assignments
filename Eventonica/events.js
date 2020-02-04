36b16a31b236659ec0d36a0df1a0814397ed409d
Zarina Gumbatova <26606767+Zarina2311@users.noreply.github.com>
2020-02-04 12:09:35 -0800

    Add more classes

--- Eventonica/events.js
+++ Eventonica/events.js
@@ -1,37 +1,65 @@
 class EventRecommender {
     constructor() {
-    // All main properties should go here.
-    
-    this.events = [];
-    this.users = [];
-    
+        this.events = [];
+        this.users = [];
     }
 
-    addEvent() {
-    // Adds a new Event to the System
+    addEvent(id, name, date, category) {
+        let newEvent = new Event(id, name, date, category);
+        this.events.push(newEvent);
     }
 
-    addUser() {
-    // Adds a new User to the System
+    addUser(id, name) {
+        let newUser = new User(id, name);
+        this.users.push(newUser);
     }
 
-    saveUserEvent(){
-    // Allow users to save events to a personal Events array.
+    saveUserEvent(user, event){
+        user.personalEvents.push(event);
     }
 
-    deleteUser() {
-    // Deletes a User from the system
+    deleteUser(id) {
+        let usersLeft = this.users.filter(function(user) {
+            return user.id !== id; 
+        });
+        this.users = usersLeft;
     }
    
-    deleteEvent() {
+    deleteEvent(id) {
     // Deletes the Event from the system
+        let eventsLeft = this.events.filter(function (event) {
+            return event.id !==id; 
+        });
+            this.events = eventsLeft;
     }
 
     findEventsByDate(){
     // Returns all events on a given date
+
     }
     
     findEventsbyCategory(){
     // Returns all events in a given category
+    
     }
 }
+
+class User {
+    constructor(id, name) {
+        this.name = name;
+        this.id = id;
+        this.personalEvents = [];
+    }
+}
+
+class Event {
+    constructor(id, name, date, category) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.category = category;
+    }
+}
+
+
+
