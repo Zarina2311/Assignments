75cef47057f8fb1379b038d7ef6d4bc0d0182661
Zarina Gumbatova <26606767+Zarina2311@users.noreply.github.com>
2020-02-05 22:15:42 -0800

    Add jQuery and html

--- Eventonica/jQuery-scripts.js
+++ Eventonica/jQuery-scripts.js
@@ -1,4 +1,98 @@
-$(document).ready( () => {
+$(document).ready(() => {
     const eventRecommender = new EventRecommender(); 
-    // Your code here
+    eventRecommender.addUser(23, "Zarina");
+    eventRecommender.addUser(11, "Lada");
+    eventRecommender.addUser(84, "Ollo");
+
+    function displayUsers() {
+        let content = $('<ul>');
+        for (let i=0; i<eventRecommender.users.length; i++) {
+            content.append("<li>" + eventRecommender.users[i].name + "</li>");
+        }
+        $("#all-users").html(content);
+    }
+    displayUsers();
+
+    $("#add-user-submit").click(function(event) {
+        // cancel refresh
+        event.preventDefault();
+        
+        // get input
+        let id = $("#add-user-id").val();
+        let name = $("#add-user-name").val();
+
+        // add user
+        eventRecommender.addUser(id, name);
+
+        // update the page
+        displayUsers();
+
+        // reset form
+        $("#add-user").trigger("reset");
+    });
+
+    $("#delete-user-id-submit").click(function(event){
+        // cancel refresh
+        event.preventDefault();
+
+        // get input
+        let id = $("#delete-user-id").val();
+
+        // delete user
+        eventRecommender.deleteUser(id);
+
+        // update the page
+        displayUsers();
+
+        // reset form
+        $("#delete-user").trigger("reset");
+    });
+
+
+    function displayEvents() {
+        let content = $('<ul>');
+        for (let i=0; i<eventRecommender.events.length; i++) {
+            content.append("<li>" + eventRecommender.events[i].name + "</li>");
+        }
+        $("#all-events").html(content);
+    }
+    displayEvents();
+
+    $("#add-event-submit").click(function(event) {
+        // cancel refresh
+        event.preventDefault();
+        
+        // get input
+        let id = $("#add-event-id").val();
+        let name = $("#add-event-name").val();
+        let date = $("#add-event-date").val();
+        let category = $("#add-event-category").val();
+
+        // add event
+        eventRecommender.addEvent(id, name, date, category);
+
+        // update the page
+        displayEvents();
+
+        // reset form
+        $("#add-event").trigger("reset");
+    });
+
+    $("#delete-event-submit").click(function(event){
+        // cancel refresh
+        event.preventDefault();
+
+        // get input
+        let id = $("#delete-event-id").val();
+
+        // delete event
+        eventRecommender.deleteEvent(id);
+
+        // update the page
+        displayEvents();
+
+        // reset form
+        $("#delete-event").trigger("reset");
+    });
+
 });
