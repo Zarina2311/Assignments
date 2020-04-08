444832e813a320e65eac710a23aef610cd0a9ce0
Zarina Gumbatova <26606767+Zarina2311@users.noreply.github.com>
2020-04-08 16:42:26 -0700

    refactoring exercise

---
+++ refactor-exercise.js
@@ -0,0 +1,41 @@
+//Exercise 1:
+
+//from
+function splitBill(amount, numPeople) {
+  return `Each person needs to pay ${amount / numPeople}`;
+}
+
+console.log(splitBill(10, 2));
+
+//to
+const splitBill = (amount, numPeople) =>
+  `Each person needs to pay ${amount / numPeople}`;
+
+console.log(splitBill(10, 2));
+
+//Exercise 2:
+
+//from
+function countdown(StartngNumber, step) {
+  let countFromNum = StartngNumber + step;
+  return function decrease() {
+    countFromNum -= step;
+    return countFromNum;
+  };
+}
+
+const countingDown = countdown(20, 2);
+
+console.log(countingDown());
+console.log(countingDown());
+
+//to
+const countdown = (startingNumber, step) => {
+  let countFromNum = startingNumber + step;
+  return () => (countFromNum -= step);
+};
+
+const countingDown = countdown(20, 2);
+
+console.log(countingDown());
+console.log(countingDown());
