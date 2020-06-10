40e08f99558973b4ea78ed462f7aeefb8244fcfe
Zarina Gumbatova <26606767+Zarina2311@users.noreply.github.com>
2020-06-10 13:34:12 -0700

    delete some notes

--- basic-javascript-practice.js
+++ basic-javascript-practice.js
@@ -4,43 +4,39 @@ outputs the result of the number of inches.
 
 Hint:1 meter equals 39.3701 inches. */
 
-
 // runtime complexity is O(1)
-function metersToInches (meters, inches) {
-    return meters * inches;
+function metersToInches(meters, inches) {
+  return meters * inches;
 }
 
 metersToInches(1.5, 39.3701);
-
 
 /* Challenge 2: Loop n Times. 
 Write a function named loopThrough that takes an argument that is a number and loops 
 through and displays a message “Hello World” that number of times. */
 
 // runtime complexity is O(x) or O(N)
-function loopThrough (x) {
-    for (let i = 0; i < x; i ++) {
-        console.log("Hello World");
-    }
+function loopThrough(x) {
+  for (let i = 0; i < x; i++) {
+    console.log("Hello World");
+  }
 }
 
 loopThrough(5);
-
 
 /* Challenge 3: Number sum.
 Define a method called calculateSum to calculate the sum all the digits from 0 to an input number. */
 
 // runtime complexity is O(n) or O(N)
 function calculateSum(n) {
-    let result = 0;
-    for (let i = 0; i <= n; i++) {
-        result += i; //result = result + i; 0+1; 1+2; 3+3; 6+4; 10+5; 15+6; 21+7; 28+8=36;
-    }
-    return result;
+  let result = 0;
+  for (let i = 0; i <= n; i++) {
+    result += i; //result = result + i; 0+1; 1+2; 3+3; 6+4; 10+5; 15+6; 21+7; 28+8=36;
+  }
+  return result;
 }
 
 calculateSum(8); // 0 + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 = 36
-
 
 /* Challenge 4: FizzBuzz.
 Write a function called fizzbuzz that prints each number from 1 to the input on a new line.
@@ -49,21 +45,20 @@ For each multiple of 5, print "Buzz" instead of the number.
 For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number. */
 
 // runtime complexity is O(newInput) or O(N)
-function fizzbuzz (newInput) {
-    for (let i = 1; i < newInput; i++) {
-        if (i % 3 === 0 && i % 5 === 0) {
-            console.log("FizzBuzz");
-        } else if (i % 3 === 0) {
-            console.log("Fizz");
-        } else if (i % 5 === 0) {
-            console.log("Buzz");
-        } else {
-            console.log(i);
-        }
-    } 
+function fizzbuzz(newInput) {
+  for (let i = 1; i < newInput; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
 }
 fizzbuzz(17);
-
 
 /* Challenge 5: Calculate Factorial Number.
 A factorial number is the concept in math by value of multiplying numbers by each consecutively smaller number.
@@ -74,12 +69,12 @@ For example, the value of 3 factorial, is 6. */
 
 // runtime complexity is O(n) or O(N)
 function factorial(n) {
-    let answer = 1;
-    for (let i = 1; i <= n; i++) {
-      answer = answer * i;
-    }
-    return answer;
+  let answer = 1;
+  for (let i = 1; i <= n; i++) {
+    answer = answer * i;
   }
+  return answer;
+}
 factorial(5);
 
 /* Challenge 6: Switch Statements. 
@@ -94,12 +89,12 @@ sleep_in(False, True) → True */
 
 // runtime complexity is O(1)
 function sleep_in(weekday, vacation) {
-    switch (!weekday || vacation) {
-        case true:
-            return true;
-        default:
-            return false;
-    }
+  switch (!weekday || vacation) {
+    case true:
+      return true;
+    default:
+      return false;
+  }
 }
 
 sleep_in(false, false); // true
@@ -117,11 +112,11 @@ common([1, 2, 3], [1, 3]) → True */
 
 // runtime complexity is O(1)
 function common(a, b) {
-    if (a[0] === b[0] || a[a.length - 1] === b[b.length - 1]) {
-        return true;
-    } else {
-        return false;
-    }
+  if (a[0] === b[0] || a[a.length - 1] === b[b.length - 1]) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 common([1, 2, 3], [7, 3]);
@@ -134,15 +129,14 @@ Delete the first key-value pair in the object. Log that object to the console ag
 
 // O(1)
 let exampleTwo = {
-    name: "Gilfoyle",
-    age: 35, 
+  name: "Gilfoyle",
+  age: 35,
 };
 
 console.log(exampleTwo);
 
 delete exampleTwo.name;
 console.log(exampleTwo);
-
 
 /* Challenge 9: Mutating Objects.
 Create an array of objects with at least 2 key value pairs. The objects should all have the same keys, 
@@ -153,24 +147,18 @@ Prove the change worked by printing the second key in each object to the console
 Use bracket-notation to access the values this time. */
 
 let myArray = [
-    {city: "San Francisco", state: "California"},
-    {city: "Nashville", state: "Tennessee"},
-    {city: "Norfolk", state: "Nebraska"}
+  { city: "San Francisco", state: "California" },
+  { city: "Nashville", state: "Tennessee" },
+  { city: "Norfolk", state: "Nebraska" },
 ];
 
 // runtime complexity is O(myArray.length) or O(N)
-function geography (myArray) {
-    for (let i = 0; i < myArray.length; i++) {
-        console.log(myArray[i].state);
-        myArray[i].state = "Florida";
-        console.log(myArray[i]["state"]);
-    }
+function geography(myArray) {
+  for (let i = 0; i < myArray.length; i++) {
+    console.log(myArray[i].state);
+    myArray[i].state = "Florida";
+    console.log(myArray[i]["state"]);
+  }
 }
 
 geography(myArray);
-
-
-/* Challenge 10: Determine Runtime Complexity.
-At the top your document for each challenge, state the runtime complexity for that function. */
-
-//done
