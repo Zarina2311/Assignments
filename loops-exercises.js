23ca3da7d48f13594056104c9ebab3bfb5750b44
Zarina Gumbatova <26606767+Zarina2311@users.noreply.github.com>
2020-06-10 13:38:23 -0700

    minor changes

--- loops-exercises.js
+++ loops-exercises.js
@@ -1,13 +1,21 @@
-/*Write a function called printVacations whose input is an array of arrays. 
-Your function should print each person's name and desired destination in a complete sentence*/
+// Write a function called printVacations whose input is an array of arrays.
+// Your function should print each person's name and desired destination in a complete sentence
 
-let vacayDestination = [["Ellen", "Hawaii"], ["Tom", "Bulgaria"], ["Michael", "Samarkand"]];
+let vacayDestination = [
+  ["Ellen", "Hawaii"],
+  ["Tom", "Bulgaria"],
+  ["Michael", "Samarkand"],
+];
 
-function printVacations () {
-    for (let i=0; i<vacayDestination.length; i++) {
-        if (vacayDestination[0][0]) {
-            console.log(vacayDestination[i][0] + " really wants to go to " + vacayDestination[i][1]);
-        }
+function printVacations() {
+  for (let i = 0; i < vacayDestination.length; i++) {
+    if (vacayDestination[0][0]) {
+      console.log(
+        vacayDestination[i][0] +
+          " really wants to go to " +
+          vacayDestination[i][1]
+      );
     }
+  }
 }
 printVacations();
