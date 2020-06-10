e73f2ad1627f843e3d4746341afce20f95236092
Zarina Gumbatova <26606767+Zarina2311@users.noreply.github.com>
2020-06-10 13:32:57 -0700

    add minor changes

--- factorial-exercise.js
+++ factorial-exercise.js
@@ -1,10 +1,10 @@
 //Factorial of 8 (8! = 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1) using recursion
 
-function factorial(num){
-    if(num === 1){
-      return num;
-    }
-    return num * factorial(num - 1);
+function factorial(num) {
+  if (num === 1) {
+    return num;
   }
-  
-  factorial(8);
+  return num * factorial(num - 1);
+}
+
+factorial(8);
